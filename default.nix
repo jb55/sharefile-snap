b30ca7b05e9645007643800eb871a40c55afcbe4
William Casarin <jb55@jb55.com>
2019-09-29 03:19:10 -0700

    nix build

---
+++ default.nix
@@ -0,0 +1,35 @@
+{ jb55pkgs ? import <jb55pkgs> {}
+, nixpkgs ? import <nixpkgs> {}
+, stdenv ? nixpkgs.stdenv
+, sharefile ? jb55pkgs.sharefile
+, maim ? nixpkgs.maim
+, libnotify ? nixpkgs.libnotify
+, makeWrapper ? nixpkgs.makeWrapper
+}:
+
+let inputs = [ sharefile maim libnotify ];
+    buildPaths = sep: fmt:
+      "${stdenv.lib.concatStringsSep sep (map fmt inputs)}";
+in stdenv.mkDerivation rec {
+  name = "snap-${version}";
+  version = "1.3";
+
+  src = ./.;
+
+  buildInputs = [ makeWrapper ] ++ inputs;
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp snap $out/bin
+
+    wrapProgram "$out/bin/snap" \
+      --prefix PATH : "${buildPaths ":" (f: "${f}/bin")}"
+  '';
+
+  meta = with stdenv.lib; {
+    description = "quick upload via sharefile";
+    homepage = "https://github.com/jb55/snap";
+    maintainers = with maintainers; [ jb55 ];
+    license = licenses.mit;
+  };
+}
